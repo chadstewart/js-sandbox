ad51d50c57a84058c75083527954d84668eb1608
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2023-07-01 01:32:13 +0000

    Just cleaning up route auth.

--- api-gateway-example/middleware/route-auth.ts
+++ api-gateway-example/middleware/route-auth.ts
@@ -5,25 +5,23 @@ import checkAuthorization from "../services/authorization";
 export default async function routeAuth (req: Request, res: Response, next: NextFunction) {
   const indexRoute = req.path === "/" && req.method === "GET";
   if(indexRoute) return next();
-  
+
+  const unauthObj = {
+    data: {
+      status: "unauthorized"
+    }
+  };
+
   const checkForAuthHeader = zod.object({ authorization: zod.string().startsWith("Basic ") });
 
   try {
     await checkForAuthHeader.parse(req.headers);
     const isAuthorized = checkAuthorization(req.headers.authorization as string);
 
-    if (!isAuthorized) return res.status(401).json({
-        data: {
-          status: "unauthorized"
-        }
-      });
+    if (!isAuthorized) return res.status(401).json(unauthObj);
 
     return next();
   } catch (err) {
-    return res.status(401).json({
-      data: {
-        status: "unauthorized"
-      }
-    });
+    return res.status(401).json(unauthObj);
   }
 };
