73fd2e557b3f9eab245af9c6d7c391c5c4d8e7c7
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2023-07-08 01:35:32 +0000

    Working on rate limit middleware. Going to test what I wrote now.

--- api-gateway-example/middleware/rate-limit.ts
+++ api-gateway-example/middleware/rate-limit.ts
@@ -1,5 +1,49 @@
 import { Request, Response, NextFunction } from "express";
 
+const REQUESTS_ALLOWED = 1;
+const TIME_LIMIT = 60000;
+
+interface RateLimitStore {
+  [key: string]: {
+    timeStamp: number;
+    totalRequests: number;
+  }
+}
+
 export default function rateLimit (req: Request, res: Response, next: NextFunction) {
+  const token = req.headers.authorization?.slice(6) as string;
+  const currentTimeStamp = Date.now();
+  const rateLimitStore: RateLimitStore = {};
+
+  const userIsInRLStore = rateLimitStore[token];
+  
+  if (!userIsInRLStore) {
+    rateLimitStore[token] = {
+      timeStamp: currentTimeStamp,
+      totalRequests: 0
+    };
+  }
+  
+  const timeStampExceeded = rateLimitStore[token]["timeStamp"] < currentTimeStamp - TIME_LIMIT;
+
+  if (timeStampExceeded) {
+    rateLimitStore[token] = {
+      timeStamp: currentTimeStamp,
+      totalRequests: 0
+    };
+  }
+
+  rateLimitStore[token]["totalRequests"]++;
+
+  const pastRateLimit = rateLimitStore[token]["totalRequests"] > REQUESTS_ALLOWED &&
+                        rateLimitStore[token]["timeStamp"] > currentTimeStamp - TIME_LIMIT;
+
+  if (pastRateLimit) {
+    return res.status(429).json({
+      status: "failed",
+      error: "Rate Limit Exceeded"
+    });
+  }
+
   next();
 };
