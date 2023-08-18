e83a3f2c43aae3d91fb369dfdce45941c2e5b364
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2023-08-18 02:03:33 +0000

    Successfully queried db and returned result using mvc architecture.

--- api-gateway-example/v1/routes/gateway-router.ts
+++ api-gateway-example/v1/routes/gateway-router.ts
@@ -2,7 +2,9 @@ import express from "express";
 const router = express.Router();
 
 import getCatImage from "../controllers/cat-controller";
+import getOrders from "../controllers/orders-controller";
 
 router.get("/cat", getCatImage);
+router.get("/order", getOrders);
 
 export default router;
