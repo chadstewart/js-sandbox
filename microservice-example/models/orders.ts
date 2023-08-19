0b1253cf418d4058b72d2dfc24821a16611dc6df
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2023-08-19 01:02:10 +0000

    Updated addPagination to allow only passing the page number as opposed to page number and row limit.

--- microservice-example/models/orders.ts
+++ microservice-example/models/orders.ts
@@ -1,9 +1,8 @@
 import { client } from "../services/database";
 import { addPagination } from "../util/pagination-helper";
-import { ROW_LIMIT } from "../util/row-limit";
 
 export const orders = (page = 1) => {
-  const paginatedQuery = addPagination(ROW_LIMIT, page);
+  const paginatedQuery = addPagination(page);
   const databaseQuery = `SELECT * FROM orders${paginatedQuery};`;
   return client.query(databaseQuery);
 };
--- microservice-example/util/pagination-helper.ts
+++ microservice-example/util/pagination-helper.ts
@@ -1,6 +1,6 @@
 import { ROW_LIMIT } from "./row-limit";
 
-export const addPagination = (inputtedRowLimit = ROW_LIMIT, pageNumber = 1) => {
+export const addPagination = (pageNumber = 1, inputtedRowLimit = ROW_LIMIT) => {
   const offsetForQuery = pageNumber * inputtedRowLimit;
   const query = " " + `LIMIT ${inputtedRowLimit} OFFSET ${offsetForQuery}`;
   return query;
