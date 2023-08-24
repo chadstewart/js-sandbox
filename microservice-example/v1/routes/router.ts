dad7b6972bddcf66b5d667f8e34cf3b3812e1547
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2023-08-24 00:23:08 +0000

    Added a route for products including a products model.

--- microservice-example/v1/routes/router.ts
+++ microservice-example/v1/routes/router.ts
@@ -4,10 +4,15 @@ const router = express.Router();
 import { getOrders, getOrderDetails } from "../controllers/orders-controller";
 import { getEmployees } from "../controllers/employees-controller";
 import { getEmployeesByTerritories } from "../controllers/territories-controller";
+import { getProducts, getProductDetails } from "../controllers/products-controller";
 
 //Orders
 router.get("/orders/:page?", getOrders);
 router.get("/orders/details/:order_id?", getOrderDetails);
+
+//Products
+router.get("/products/:page?", getProducts);
+router.get("/products/details/:product_id?", getProductDetails);
 
 //Employees
 router.get("/employees/:page?", getEmployees);
