3e0d317ef3fdf1d4b2dec506873bec5caf2f89f5
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2023-08-29 02:22:35 +0000

    Added addEmployee model to project. Not tested if the sql query actually works yet.

--- microservice-example/models/employees.ts
+++ microservice-example/models/employees.ts
@@ -1,5 +1,6 @@
 import { client } from "../services/database";
 import { addPagination } from "../util/pagination-helper";
+import { createEmployeeZodSchema } from "../util/schemas/employeeZodSchema";
 import { totalPaginationPages } from "../util/total-pagination-pages";
 
 export const employees = async (page = 1) => {
@@ -22,4 +23,46 @@ export const employees = async (page = 1) => {
     totalPages
   };
   return data;
+};
+
+export const addEmployee = async (reqBody: any) => {
+  try {
+    const createEmployeeSchema = await createEmployeeZodSchema.parse(reqBody);
+
+    const {
+      employee_id,
+      last_name,
+      first_name,
+      title,
+      title_of_courtesy,
+      birth_date,
+      hire_date,
+      address,
+      city,
+      region,
+      postal_code,
+      country,
+      home_phone,
+      extension,
+      photo,
+      notes,
+      reports_to,
+      photo_path,
+      territory_id
+    } = createEmployeeSchema;
+    
+    const databaseQuery = 
+    `BEGIN;
+    INSERT INTO
+      employees (employee_id, last_name, first_name, title, title_of_courtesy, birth_date, hire_date, address, city, region, postal_code, country, home_phone, extension, photo, notes, reports_to, photo_path)
+    VALUES (${employee_id}, ${last_name}, ${first_name}, ${title}, ${title_of_courtesy}, ${birth_date}, ${hire_date}, ${address}, ${city}, ${region}, ${postal_code}, ${country}, ${home_phone}, ${extension}, ${photo}, ${notes}, ${reports_to}, ${photo_path});
+    INSERT INTO
+      employee_territories (employee_id, territory_id)
+    VALUES (${employee_id}, ${territory_id});
+    COMMIT;`;
+    const queryData = await client.query(databaseQuery);
+    return queryData;
+  } catch (error) {
+    throw error;
+  }
 };
