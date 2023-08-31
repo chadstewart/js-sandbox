a52f6393514955b7b44216fc0fded93b6c7f774d
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2023-08-31 01:06:26 +0000

    Continuing to work on adding orders to database. Added the schema for both adding orders with a new customer and an order with an existing customer

---
+++ microservice-example/util/schemas/addOrdersZodSchema.ts
@@ -0,0 +1,36 @@
+import zod from "zod";
+
+export const addOrdersZodSchema = zod.object({
+  orders: zod.object({
+    customer_id: zod.string().optional(),
+    employee_id: zod.string(),
+    order_date: zod.string(),
+    required_date: zod.string(),
+    shipped_date: zod.string(),
+    ship_via: zod.string(),
+    frieght: zod.string(),
+    ship_name: zod.string(),
+    ship_address: zod.string(),
+    ship_city: zod.string(),
+    ship_region: zod.string(),
+    ship_postal_code: zod.string(),
+    ship_country: zod.string(),
+  }),
+  order_details: zod.object({
+    unit_price: zod.string(),
+    quantity: zod.string(),
+    discount: zod.string(),
+  }),
+  customers: zod.object({
+    company_name: zod.string(),
+    contact_name: zod.string(),
+    contact_title: zod.string(),
+    address: zod.string(),
+    city: zod.string(),
+    region: zod.string(),
+    postal_code: zod.string(),
+    country: zod.string(),
+    phone: zod.string(),
+    fax: zod.string()
+  }).partial()
+});
