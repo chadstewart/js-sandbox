c49b86d5ade3e14a986df1baf6c61beea1a447ce
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2023-09-08 23:12:06 +0000

    Updated customer and customer details to using prisma.

--- microservice-orm-example/models/customers.ts
+++ microservice-orm-example/models/customers.ts
@@ -1,21 +1,20 @@
-import { client } from "../services/database";
-import { addPagination } from "../util/pagination-helper";
-import { totalPaginationPages } from "../util/total-pagination-pages";
+import { client, prisma } from "../services/database";
+import { prismaPaginationHelper } from "../util/pagination-helper";
 import { updateCustomerZodSchema } from "../util/schemas/update-customer-zod-schema";
 
 export const customers = async (page = 1) => {
-  const paginatedQuery = addPagination(page);
-  const databaseQuery =
-  `SELECT
-      customer_id,
-      company_name,
-      contact_name,
-      contact_title
-    FROM
-      customers
-    ${paginatedQuery};`;
-  const queryData = await client.query(databaseQuery);
-  const totalPages = await totalPaginationPages("product_id", "products");
+  const { skip, take } = prismaPaginationHelper(page);
+  const queryData = prisma.customers.findMany({
+    select: {
+      customer_id: true,
+      company_name: true,
+      contact_name: true,
+      contact_title: true
+    },
+    skip,
+    take
+  });
+  const totalPages = prisma.customers.count();
   const data = {
     ...queryData,
     totalPages
@@ -24,22 +23,22 @@ export const customers = async (page = 1) => {
 };
 
 export const customerDetails = async (customerId: string) => {
-  const databaseQuery =
-  `Select
-      customers.customer_id,
-      company_name,
-      contact_name,
-      contact_title,
-      customer_desc    
-    From
-      customers
-    LEFT JOIN
-      customer_customer_demo on customers.customer_id=customer_customer_demo.customer_id
-    LEFT JOIN
-      customer_demographics on customer_customer_demo.customer_type_id=customer_demographics.customer_type_id
-    WHERE
-      customers.customer_id='${customerId}';`;
-  const queryData = await client.query(databaseQuery);
+  const queryData = prisma.customers.findMany({
+    select: {
+      customer_id: true,
+      company_name: true,
+      contact_name: true,
+      contact_title: true,
+      customer_customer_demo: {
+        select: {
+          customer_demographics: true
+        }
+      }
+    },
+    where: {
+      customer_id: customerId
+    }
+  });
   return queryData;
 };
 
