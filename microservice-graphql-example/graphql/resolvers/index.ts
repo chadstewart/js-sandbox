e8a2b1eba442662dced96b898f965e0054d60d85
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2023-09-20 01:15:29 +0000

    Added resolvers for basic GraphQL schema.

--- microservice-graphql-example/graphql/resolvers/index.ts
+++ microservice-graphql-example/graphql/resolvers/index.ts
@@ -1,14 +1,22 @@
+import { categories } from "../../models/categories";
+import { customerDetails, customers } from "../../models/customers";
+import { employees } from "../../models/employees";
+import { orderDetails, orders } from "../../models/orders";
+import { products } from "../../models/products";
+import { supplier } from "../../models/suppliers";
+
 const book = { title: "hello", author: "world" }
 
 export const resolvers = {
   Query: {
     book: () => book,
-    getOrders: () => null,
-    getOrderDetails: () => null,
-    getEmployees: () => null,
-    getCustomers: () => null,
-    getProducts: () => null,
-    getCategories: () => null,
-    getSuppliers: () => null
+    getOrders: (page: number) => orders(page),
+    getOrderDetails: (id: number) => orderDetails(id),
+    getEmployees: (page: number) => employees(page),
+    getCustomers: (page: number) => customers(page),
+    getCustomerDetails: (id: string) => customerDetails(id),
+    getProducts: (page: number) => products(page),
+    getCategories: (page: number) => categories(page),
+    getSuppliers: (page: number) => supplier(page)
   },
 };
---
+++ microservice-graphql-example/models/categories.ts
@@ -0,0 +1,16 @@
+import { prismaPaginationHelper } from "../util/pagination-helper";
+import { prisma } from "../services/database";
+
+export const categories = async ( page = 1 ) => {
+  const { skip, take } = prismaPaginationHelper(page);
+  const queryData = await prisma.categories.findMany({
+    skip,
+    take
+  });
+  const totalRows = await prisma.categories.count();
+  const data = {
+    queryData,
+    totalRows
+  };
+  return data;
+}
