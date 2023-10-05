a860aae62fed0a44cccc5132b6662f485d745b37
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2023-10-05 23:28:07 +0000

    Implemented winston logger from you-go backend project.

---
+++ microservice-graphql-example/services/logger/prod-logger.ts
@@ -0,0 +1,15 @@
+import { format, createLogger, transports } from 'winston';
+import path from 'path';
+const { timestamp, combine, errors, json } = format;
+
+function buildProdLogger() {
+  return createLogger({
+    format: combine(timestamp(), errors({ stack: true }), json()),
+    transports: [
+      new transports.File({ filename: path.join(__dirname, "../../logs/", "general.log") }),
+      new transports.File({ filename: path.join(__dirname, "../../logs/", "error.log"), level: "error" }),
+    ],
+  });
+}
+
+export default buildProdLogger;
