27ca7c166ed208915165088fd5e649de8bfd7c20
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2023-10-07 00:35:27 +0000

    Removed SQL Injection vulnerability from territories model.

--- microservice-example/models/territories.ts
+++ microservice-example/models/territories.ts
@@ -3,7 +3,7 @@ import { addPagination } from "../util/pagination-helper";
 import { totalPaginationPages } from "../util/total-pagination-pages";
 
 export const employeeFromTerritories = async (page = 1, territoryId = 1) => {
-  const paginatedQuery = addPagination(page);
+  const { inputtedRowLimit, offsetForQuery } = addPagination(page);
   const databaseQuery =
   `Select
       employee_territories.territory_id,
@@ -24,9 +24,10 @@ export const employeeFromTerritories = async (page = 1, territoryId = 1) => {
     LEFT JOIN
       region on territories.region_id=region.region_id
     WHERE
-      employee_territories.territory_id='${territoryId}'
-    ${paginatedQuery};`;
-  const queryData = await client.query(databaseQuery);
+      employee_territories.territory_id=$1
+    LIMIT $2 OFFSET $3;`;
+  const databaseQueryValues = [territoryId, inputtedRowLimit, offsetForQuery];
+  const queryData = await client.query(databaseQuery, databaseQueryValues);
   const totalPages = await totalPaginationPages("employee_id", "employees");
   const data = {
     ...queryData,
