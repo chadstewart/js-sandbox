b06423a8c4a610832614b4cf3c4eb08b25535af9
Chad Stewart <chadrhonanstewart@gmail.com>
2024-07-05 16:44:26 -0500

    Organizing JS Sandbox into exploratory projects and projects to practice fundamentals.

---
+++ exploratory-projects/techishiring-sandbox/src/images/Index.tsx
@@ -0,0 +1,15 @@
+import contactImg from "./contact-image.png";
+import contactMobileImg from "./contact-image-mobile.png";
+import aboutImage from "./about-image1.png";
+import aboutMobileImage from "./about-image2.png";
+import HeroImage from "./heroImage.svg";
+import ChadProfilePic from "./chad-profile-pic.jpg";
+
+export {
+  contactImg,
+  contactMobileImg,
+  aboutImage,
+  aboutMobileImage,
+  HeroImage,
+  ChadProfilePic,
+};
